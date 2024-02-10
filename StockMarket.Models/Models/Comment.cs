ff40f65bf1dc4902a361682b42fe0075add69e1a
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2024-02-10 18:24:07 +0200

    Adding Models With Navigtion Properties

---
+++ StockMarket.Models/Models/Comment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket.Models.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Title length must be between 1 and 100 characters")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Content is required")]
+        [StringLength(500, MinimumLength = 1, ErrorMessage = "Content length must be between 1 and 500 characters")]
+        public string Content { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Created on date is required")]
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+
+        // navigation property
+        [ForeignKey("Stock")]
+        public int StockId { get; set; }
+        public Stock? Stock { get; set; }
+
+
+    }
+}
--- StockMarket.Models/Profiles/MappingProfiles.cs
+++ StockMarket.Models/Profiles/MappingProfiles.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace StockMarket.Models.Profiles
 {
-    internal class MappingProfiles
+    public class MappingProfiles : Profile
     {
+        public MappingProfiles()
+        {
+                
+        }
     }
 }
