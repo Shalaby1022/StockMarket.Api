5f3ece978aa3ccd6690da0653b2818f71cfc9c4c
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2024-02-25 23:52:40 +0200

    Added a RegisterDto class to encapsulate user registration data transfer objects.

---
+++ StockMarket.Models/DTO's/AccountDtos/RegisterDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket.Models.DTO_s.AccountDtos
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "First name is required")]
+        public string FName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string LName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        [PasswordPropertyText]
+        public string Password { get; set; } = string.Empty;
+
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+
+    }
+}
