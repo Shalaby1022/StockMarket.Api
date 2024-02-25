eb160e137f90851dce79c3b5ee2aabab908ddf39
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2024-02-25 23:52:59 +0200

    Implement manual mapping extension method for user registration

---
+++ StockMarket.Models/ManualMappingUsingExtensionMethods/AccountMapping.cs
@@ -0,0 +1,26 @@
+﻿using StockMarket.Models.DTO_s.AccountDtos;
+using StockMarket.Models.DTO_s.CommentDtos;
+using StockMarket.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket.Models.ManualMappingUsingExtensionMethods
+{
+    public static class AccountMapping
+    {
+        public static ApplicationUser ToAppUSerFromCreation(this RegisterDto registerDto)
+        {
+            return new ApplicationUser
+            {
+                FName = registerDto.FName,
+                LName = registerDto.LName,
+                UserName = registerDto.UserName,
+                Email = registerDto.Email,
+            };
+        }
+
+    }
+}
