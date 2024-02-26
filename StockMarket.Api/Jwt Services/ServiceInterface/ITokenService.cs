50ea18ea3f6b7728b6e258c64ea01e69f99a3f7c
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2024-02-26 21:30:48 +0200

    Implementing Token Service for JWT tokens and Password Hashing

---
+++ StockMarket.Api/Jwt Services/ServiceInterface/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using StockMarket.Models.Models;
+
+namespace StockMarket.Api.Jwt_Services.ServiceInterface
+{
+    public interface ITokenService
+    {
+     string CreateToken(ApplicationUser applicationUser);
+
+    }
+}
